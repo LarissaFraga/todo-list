11e6bda7715b061a8eca7458d999b45d3f468a2e
Larissa <larissa.fraga@ufms.br>
2022-10-16 18:22:15 -0400

    first commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <InputText v-model="description" />
+  <div class="div-buttons">
+    <Button @click="cancel">Back</Button>
+    <Button @click="save">Save</Button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      description: "",
+    };
+  },
+  props: {
+    todo: Object,
+  },
+  created() {
+    if (this.todo) {
+      this.description = this.todo.description;
+    }
+  },
+  methods: {
+    cancel() {
+      this.$emit("cancel");
+    },
+    save() {
+      this.$emit("saveTodo", { ...this.todo, description: this.description });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.div-buttons {
+  margin-top: 1rem;
+  display: flex;
+  gap: 1rem;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="todo-list">
+    <div v-for="(todo, index) in todos" :key="index" class="todo-item">
+      <label class="todo-label">{{ todo.description }}</label>
+      <Button
+        icon="pi pi-pencil"
+        class="p-button-rounded p-button-text"
+        @click="$emit('editTodo', index)"
+      />
+      <Button
+        icon="pi pi-trash"
+        class="p-button-rounded p-button-text"
+        @click="$emit('deleteTodo', index)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.todo-list {
+  margin-top: 1rem;
+}
+.todo-item {
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid lightgray;
+}
+.todo-label {
+  flex: 1;
+}
+</style>
